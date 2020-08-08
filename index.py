b090f65bf27b572a76da413490a02ddb9d5cf7cf
Jose Silva <46269951+josesilva27@users.noreply.github.com>
2020-08-08 15:01:52 -0400

    Add files via upload

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os               
-import zipfile
+from zipfile import ZipFile
+import shutil
 from flask import Flask , request, render_template
 from werkzeug.utils import secure_filename
 
@@ -14,6 +15,7 @@ def archivos():
     return render_template ("archivos.html")
 
 app.config['UPLOAD_FOLDER'] = './subidos'
+app.config['DOWNLOAD_FOLDER'] = './Descargas'
 
 @app.route("/uploader", methods=['POST'])
 def uploader():
@@ -22,7 +24,30 @@ def uploader():
         filename = secure_filename(f.filename)
         f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+        if os.path.exists("Descargas") == True:
+            if len(os.listdir(os.getcwd()+'\\Descargas')) == 0:
+                print("carpeta vacia")
+        else:
+            os.mkdir ("Descargas")
+
+        rutaSubidos= os.getcwd()+'\\subidos'
+        rutaDescargas = os.getcwd()+'\\Descargas'
+        archivo = open(rutaSubidos+'\\'+ filename,'r')
+        
+        myzip = ZipFile("archivo.zip",'w')
+        myzip.write (rutaSubidos+'\\'+filename, os.path.basename(filename)) #busco el archivo en la carpeta subidos
+        myzip.close()
+        print("zip creado")
+        archivo.close()
+
+        shutil.move("archivo.zip", rutaDescargas)
+        print("archivo fue enviado a descargas")
+
+        with ZipFile(rutaDescargas+'\\archivo.zip','r') as myzip:
+            myzip.extractall()
+
         return 'subido exitosamente'
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
